f53488979476a2dcf6cef53d86622909524f8389
brunocpp <brunocp.souto@gmail.com>
2026-03-14 14:38:36 -0300

    feat: add basic student data input and output

---
+++ src/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void) {
+
+    char name[100];
+    char student_id[20];
+    float grade;
+
+    printf("Enter your name?:");
+    scanf("%s", name);
+
+    printf("Enter your student id?:");
+    scanf("%s", student_id);
+
+    printf("Enter your grade?:");
+    scanf("%f", &grade);
+
+    printf("Welcome student!\n");
+    printf("student name:%s\n", name);
+    printf("student id:%s\n", student_id);
+    printf("student grade:%.2f\n", grade);
+
+    return 0;
+}
