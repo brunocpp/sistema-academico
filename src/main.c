a2670f429e60a13df29472e6f371a0b148fe7f9d
brunocpp <brunocp.souto@gmail.com>
2026-03-25 21:24:42 -0300

    feat:add Student struct

--- src/main.c
+++ src/main.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
+#include "student.h"
 
-int main(void) {
+void print_student(Student *s1) {
+    printf("Welcome student!\n");
+    printf("Student name: %s\n", s1->name);
+    printf("Student id: %s!\n", s1->student_id);
+    printf("Student grade: %.1f!\n", s1->grade);
+}
 
-    char name[100];
-    char student_id[20];
-    float grade;
+int main() {
+    Student s1;
 
     printf("Enter your name?:");
-    scanf(" %[^\n]", name);
+    scanf(" %[^\n]", s1.name);
 
     printf("Enter your student id?:");
-    scanf("%s", student_id);
+    scanf("%s", s1.student_id);
 
     printf("Enter your grade?:");
-    scanf("%f", &grade);
+    scanf("%f", &s1.grade);
 
-    printf("Welcome student!\n");
-    printf("student name:%s\n", name);
-    printf("student id:%s\n", student_id);
-    printf("student grade:%.2f\n", grade);
+    print_student(&s1);
 
     return 0;
 }
---
+++ src/student.h
@@ -0,0 +1,11 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+typedef struct {
+    char name[100];
+    char student_id[20];
+    float grade;
+} Student;
+
+#endif
+
