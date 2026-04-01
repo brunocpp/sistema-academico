4304c6bf6f05d342fc080448b1f1f804ce90b26b
brunocpp <brunocp.souto@gmail.com>
2026-04-01 20:29:20 -0300

    feat: add student array with cadastro and listagem

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "student.h"
+#define MAX 50
 
 void print_student(Student *s1) {
     printf("Welcome student!\n");
@@ -9,18 +10,27 @@ void print_student(Student *s1) {
 }
 
 int main() {
-    Student s1;
 
-    printf("Enter your name?:");
-    scanf(" %[^\n]", s1.name);
+    Student students[MAX];
+    int total_students = 0;
 
-    printf("Enter your student id?:");
-    scanf("%s", s1.student_id);
+    for (int i = 0; i < 3; i++) {
+        printf("Enter your name?:");
+        scanf(" %[^\n]", students[total_students].name);
 
-    printf("Enter your grade?:");
-    scanf("%f", &s1.grade);
+        printf("Enter your student id?:");
+        scanf("%s", students[total_students].student_id);
 
-    print_student(&s1);
+        printf("Enter your grade?:");
+        scanf("%f", &students[total_students].grade);
+
+        total_students++;
+    }
+
+
+    for (int i = 0; i < total_students; i++) {
+        print_student(&students[i]);
+    }
 
     return 0;
 }
